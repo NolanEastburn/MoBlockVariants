14534a22ca9d0a4b08c9ada3ba7672f5db7766bb
NolanEastburn <nolieman1@gmail.com>
2017-08-14 13:07:09 -0500

    Staged more necesary files

--- src/main/java/com/mbhabesNolieman108/MBV/init/ModItems.java
+++ src/main/java/com/mbhabesNolieman108/MBV/init/ModItems.java
@@ -2,32 +2,42 @@ package com.mbhabesNolieman108.MBV.init;
 
 import com.mbhabesNolieman108.MBV.reference;
 import com.mbhabesNolieman108.MBV.items.ItemChunk;
+import com.mbhabesNolieman108.MBV.items.ItemDBSword;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-public class ModItems {
+public class ModItems 
+{
 	
 	public static Item chunk;
+	public static Item DBSword;
 	
-	public static void init( ) {
+	public static void init( ) 
+	{
 		chunk = new ItemChunk();
+		DBSword = new ItemDBSword();
 		
 	}
 	
-	public static void register() {
+	public static void register() 
+	{
 		GameRegistry.register(chunk);
+		GameRegistry.register(DBSword);
 		
 	}
 	
-	public static void registerRenders() {
+	public static void registerRenders() 
+	{
 		registerRender(chunk);
+		registerRender(DBSword);
 	
 	}
 	
-	private static void registerRender(Item item) {
+	private static void registerRender(Item item) 
+	{
 		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
 		
 	}
